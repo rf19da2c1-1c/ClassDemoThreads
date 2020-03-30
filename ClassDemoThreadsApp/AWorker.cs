53429a0f669d3056f315073811b4c975f58771da
Peter Levinsky <pele@easj.dk>
2020-03-30 10:15:22 +0200

    Add project files.

---
+++ ClassDemoThreadsApp/AWorker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClassDemoThreadsApp
+{
+    internal class AWorker
+    {
+        public void Start()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+
+                Thread t = new Thread(() => PrintNoget(i));
+                t.Start();
+
+                //Task.Run(() =>
+                //{
+                //    int k = i;
+                //    PrintNoget(k);
+                //});
+            }
+
+
+
+
+        }
+
+
+        protected void PrintNoget(int i)
+        {
+            for (int j = 0; j < 50; j++)
+            {
+                Console.WriteLine($"Dette er tråd nummer {i}");
+
+            }
+
+        }
+    }
+}
---
+++ ClassDemoThreadsApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoThreadsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AWorker worker = new AWorker();
+            worker.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
